89d8178e02547966afaf08eeec30c907eded2e77
Urhoghide Justin Uyioghosa <uyijustindavid@gmail.com>
2025-04-15 04:24:01 +0100

    fix: security

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -8,15 +8,16 @@ import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDHsLw1h6vkH7MolF5rr4MZealZvhisoMQ",
-  authDomain: "stack-links.firebaseapp.com",
-  projectId: "stack-links",
-  storageBucket: "stack-links.firebasestorage.app",
-  messagingSenderId: "983683858263",
-  appId: "1:983683858263:web:861ff133cc8a7e47668812",
-  measurementId: "G-7EYECMYYRM",
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
